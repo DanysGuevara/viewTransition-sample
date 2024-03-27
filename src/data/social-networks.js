3a39dd59fa7f1ba038955d21d25044818c66d73b
DanysGuevara <ga19014@ues.edu.sv>
2024-03-26 23:28:27 -0600

    v1 diseño iniciado header y footer responsive

---
+++ src/data/social-networks.js
@@ -0,0 +1,20 @@
+export const socialNetworks = [
+    {
+        id: "github",
+        url: "https://github.com/DanysGuevara",
+        icon: "brand-github.svg",
+        alt: "icono de GitHub",
+    },
+    {
+        id: "facebook",
+        url: "https://www.facebook.com/Enmanuel.Arriaza/",
+        icon: "brand-facebook.svg",
+        alt: "icono de Facebook",
+    },
+    {
+        id: "youtube",
+        url: "https://www.youtube.com/@midulive",
+        icon: "brand-youtube.svg",
+        alt: "icono de YouTube",
+    }
+]
