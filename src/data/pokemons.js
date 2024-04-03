02b55fb70bb6168f6332ee01b53fb2b7253b16fd
DanysGuevara <ga19014@ues.edu.sv>
2024-04-03 16:17:25 -0600

    Añadir componente CardPokemon y estilos relacionados, y actualizar Layout y página principal

---
+++ src/data/pokemons.js
@@ -0,0 +1,29 @@
+const pokemons = [
+    {
+        hp: "120",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        pokeName: "Bulbasaur",
+        statAttack: "49",
+        statDefense: "45",
+        statSpeed: "95",
+        type: "Grass"
+    },
+    {
+        hp: "120",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+        pokeName: "Charmander",
+        statAttack: "52",
+        statDefense: "43",
+        statSpeed: "65",
+        type: "Fire"
+    },
+    {
+        hp: "120",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+        pokeName: "Squirtle",
+        statAttack: "48",
+        statDefense: "65",
+        statSpeed: "43",
+        type: "Water"
+    }
+];
